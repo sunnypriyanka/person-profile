11c94952d19f6bf002d2adfe00ac6ca00cfe66ba
sunny priyanka <sunnypriyanka28@gmail.com>
2021-09-16 23:22:00 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import SplitButton from './Splitbutton'
 
-import Button from '@material-ui/core/Button';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <SplitButton></SplitButton> */}
+  
     <App />
   </React.StrictMode>,
   document.getElementById('root')
